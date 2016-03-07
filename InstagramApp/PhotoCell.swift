35adbf2031e09267d81638aaf0f5209c327eae0c
Julia Pettere <juliapettere@D-10-157-62-208.dhcp4.washington.edu>
2016-03-06 21:31:10 -0800

    added all required and optional features

---
+++ InstagramApp/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  InstagramApp
+//
+//  Created by Julia Pettere on 3/6/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ InstagramApp/UserMedia.swift
@@ -0,0 +1,13 @@
+//
+//  UserMedia.swift
+//  InstagramApp
+//
+//  Created by Julia Pettere on 3/6/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class UserMedia: NSObject {
+
+}
